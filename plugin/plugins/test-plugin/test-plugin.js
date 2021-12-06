e39d1e1d1e57261e451c626f81825b19796e20ac
lovestories0eng <1494121350@qq.com>
2021-12-06 23:07:40 +0800

    first commit

---
+++ plugin/plugins/test-plugin/test-plugin.js
@@ -0,0 +1,2 @@
+/*! For license information please see test-plugin.js.LICENSE.txt */
+!function(){var e=function(e){jQuery,e.testPlugin=function(){alert("testPlugin")},e.fn.testPluginMethodA=function(){alert("testPluginMethodA")}};"function"==typeof require&&"object"==typeof exports&&"object"==typeof module?module.exports=e:"function"==typeof define?define.amd?define(["editormd"],(function(t){e(t)})):define((function(t){var n=t("./../../editormd");e(n)})):e(window.editormd)}();
