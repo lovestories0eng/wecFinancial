6694d4345e861ea802a5e576bf341d406d1e827d
lovestories0eng <1494121350@qq.com>
2021-12-11 22:56:15 +0800

    delete plugin

--- plugin/plugins/plugin-template.js
+++
@@ -1,2 +0,0 @@
-/*! For license information please see plugin-template.js.LICENSE.txt */
-!function(){var e=function(e){jQuery,e.fn.htmlEntities=function(){}};"function"==typeof require&&"object"==typeof exports&&"object"==typeof module?module.exports=e:"function"==typeof define?define.amd?define(["editormd"],(function(n){e(n)})):define((function(n){var o=n("./../../editormd");e(o)})):e(window.editormd)}();
